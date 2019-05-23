a89652f96d9642f47c2c4771000f20c8467673a9
Yifei <yifei.lu.chn@gmail.com>
2019-05-23 16:41:40 +0200

    make changes to py script

--- graphics/python_scripts/pkgInstaller.py
+++ graphics/python_scripts/pkgInstaller.py
@@ -1,5 +1,8 @@
 import os
 from pathlib import Path
+import requests
+import urllib.request
+from bs4 import BeautifulSoup
 
 
 path = str(Path(os.path.abspath(__file__)).parent.parent.parent)
@@ -15,6 +18,26 @@ with open(filePath) as f:
             print(pkgName)
             pkgList.append(pkgName)
 
+new_pkgList = []
+for pkg in pkgList:
+    url = 'https://ctan.org/pkg/' + pkg
+    r = requests.get(url)
+    r.encoding = r.apparent_encoding
+    html_content = r.text
+    soup = BeautifulSoup(html_content, "html.parser")
+    for tr in soup.find_all('tr'):
+        if 'X Live</a>' in str(tr):
+            pkgName = str(tr).split('<br')[0].split(' ')[-1]
+            if pkgName not in new_pkgList:
+                new_pkgList.append(pkgName)
+
+# TODO some package names contain extra hyphens
+
+stringOfPkg = new_pkgList[0]
+for pkg in new_pkgList[1:]:
+    stringOfPkg = stringOfPkg + ', ' + pkg
+
 os.system('tlmgr --version')
 os.system('tlmgr update --self')
+os.system('tlmgr install ' + stringOfPkg)
 
